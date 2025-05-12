443516626d358fbe2be4f6749b59f200dfde5bd6
beer-psi <lacvtg.a1.2023@gmail.com>
2025-05-12 12:26:52 +0700

    fix(ci): stop copy pasting shit

--- check_stable.py
+++ check_stable.py
@@ -13,7 +13,7 @@ import toml
 import sys
 
 # Dockerhub repo to compare rust-lang release with
-DOCKERHUB_REPO="clux/muslrust"
+DOCKERHUB_REPO="beerpsi/cargo-chef-musl-mimalloc"
 
 def rust_stable_version():
     """Retrieve the latest rust stable version from static.rust-lang.org"""
